4c34e341f590a92208f5b930f471097c3f5608da
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-02-15 11:31:27 -0500

    Add tests for alignment with TRAPI schema

---
+++ tests/test_openapi.py
@@ -0,0 +1,74 @@
+"""Test alignment with OpenAPI schema."""
+import json
+
+import httpx
+import yaml
+
+from reasoner_pydantic import components
+
+
+class NormDecoder(json.JSONDecoder):
+    """JSON decoder that reduces schemas the their functional core."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize."""
+        json.JSONDecoder.__init__(
+            self,
+            object_hook=self.object_hook,
+            *args,
+            **kwargs,
+        )
+
+    def object_hook(self, dct):
+        """Object hook."""
+        if "$ref" in dct:
+            if dct["$ref"].startswith("#/definitions/"):
+                ref = "#/components/schemas" + dct["$ref"][13:]
+            else:
+                ref = dct["$ref"]
+            return {
+                "$ref": ref
+            }
+
+        # This is potentially problematic in general... #
+        if "anyOf" in dct:
+            dct["oneOf"] = dct.pop("anyOf")
+        #################################################
+
+        dct.pop("title", None)
+        dct.pop("description", None)
+        dct.pop("example", None)
+        dct.pop("externalDocs", None)
+        if dct.get("additionalProperties", None) is True:
+            dct.pop("additionalProperties")
+        if list(dct.keys()) == ["allOf"]:
+            if len(dct["allOf"]) == 1:
+                return dct["allOf"][0]
+        return dct
+
+
+response = httpx.get("https://raw.githubusercontent.com/NCATSTranslator/ReasonerAPI/v1.0.3/TranslatorReasonerAPI.yaml")
+reference_schemas = yaml.load(
+    response.text,
+    Loader=yaml.FullLoader,
+)["components"]["schemas"]
+reference_schemas = json.loads(
+    json.dumps(reference_schemas),
+    cls=NormDecoder,
+)
+
+
+def test_openapi():
+    """Test alignment with OpenAPI schema."""
+    for obj in components:
+        print(obj.__name__)
+
+        schema = json.loads(obj.schema_json(), cls=NormDecoder)
+
+        schema.pop("definitions", None)
+        try:
+            assert schema == reference_schemas[obj.__name__]
+        except AssertionError:
+            print("  produced schema: ", schema)
+            print("  reference schema: ", reference_schemas[obj.__name__])
+            raise
