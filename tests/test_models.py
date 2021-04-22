ea653cbb962ce36ba6771b2c722460db23678e6a
Alon Greyber <alon@greyber.org>
2021-04-22 14:16:59 -0400

    Added test for null properties

---
+++ tests/test_models.py
@@ -0,0 +1,16 @@
+from reasoner_pydantic import QNode, QEdge
+
+def test_qnode_null_properties():
+    """ Check that we can parse a QNode with None property values """
+    QNode.parse_obj({
+        "id": None,
+        "categories": None,
+    })
+
+def test_qedge_null_properties():
+    """ Check that we can parse a QEdge with None property values """
+    QEdge.parse_obj({
+        "subject" : "n0",
+        "object" : "n1",
+        "predicate": None,
+    })
