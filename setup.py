f7f0411559d6bc420077ef8e46af34e94fe454ee
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-06-18 09:51:33 -0400

    Bump library version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="reasoner-pydantic",
-    version="1.1.1",
+    version="1.1.2",
     author="Patrick Wang",
     author_email="patrick@covar.com",
     url="https://github.com/TranslatorSRI/reasoner-pydantic",
