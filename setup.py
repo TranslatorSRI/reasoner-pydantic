dc2656df089a83313bc29d66403cd8b9eeccd912
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-09-15 11:25:06 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="reasoner-pydantic",
-    version="1.2.0.3",
+    version="1.2.0.4",
     author="Patrick Wang",
     author_email="patrick@covar.com",
     url="https://github.com/TranslatorSRI/reasoner-pydantic",
