293a2b9c74a60dad4ce2ce32951ca258d0448dff
Abrar Mesbah <abrar.mesbah@gmail.com>
2022-03-21 13:53:30 -0400

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="reasoner-pydantic",
-    version="2.1.2",
+    version="2.2.0",
     author="Kenneth Morton",
     author_email="kenny@covar.com",
     url="https://github.com/TranslatorSRI/reasoner-pydantic",
