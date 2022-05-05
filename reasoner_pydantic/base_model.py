559f5de7fc0f688064e55ef074a5391b19423a24
Kenneth Morton <kenny@covar.com>
2022-05-05 14:01:05 -0500

    Tests pass with extras that are lists and dicts

--- reasoner_pydantic/base_model.py
+++ reasoner_pydantic/base_model.py
@@ -1,7 +1,7 @@
-from typing import Callable, Optional
-import weakref
-from pydantic import BaseModel as PydanticBaseModel, PrivateAttr
+from pydantic import BaseModel as PydanticBaseModel
 
+from pydantic.class_validators import root_validator
+from .utils import HashableMapping, HashableSequence, make_hashable
 
 class BaseModel(PydanticBaseModel):
     """
@@ -23,3 +23,28 @@ class BaseModel(PydanticBaseModel):
         raise NotImplementedError(
             f"Model {self.__class__.__name__} has no update method"
         )
+
+    def make_hashable(cls, values):
+        """
+        Convert a generic Python object to a hashable one recursively
+
+        This is an expensive operation, so it is best used sparingly
+        """
+
+        # type(o) is faster than isinstance(o) because it doesn't
+        # traverse the inheritance hierarchy
+        o_type = str(type(values))
+        
+        new_value = values
+        if "dict" in o_type:
+            new_value = HashableMapping.parse_obj(((k, cls.make_hashable(v)) for k, v in values.items()))
+        if "list" in o_type:
+            new_value = HashableSequence.parse_obj(cls.make_hashable(v) for v in values)
+
+        return new_value
+
+    @root_validator(allow_reuse=True, pre=False)
+    def make_hashable_root(cls, values):
+        # The root validator must take and return a dict
+        return {k: make_hashable(k) for k, v in values.items()}
+
