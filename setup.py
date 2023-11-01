0bd1e397b71408e8d534bc5d509a74392e1e4d13
Abrar Mesbah <abrar.mesbah@gmail.com>
2023-11-01 13:12:46 -0400

    set max pydantic version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     long_description_content_type="text/markdown",
     packages=["reasoner_pydantic"],
     include_package_data=True,
-    install_requires=["pydantic>=1.8"],
+    install_requires=["pydantic>=1.8,<=1.10"],
     zip_safe=False,
     license="MIT",
     python_requires=">=3.6",
