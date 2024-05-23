0d4a6ce87dd5ccf0c8f555d7ed5e1bd155b3e1c0
uhbrar <abrar.mesbah@gmail.com>
2024-05-23 15:25:54 -0400

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="reasoner-pydantic",
-    version="5.0.2",
+    version="5.0.3",
     author="Abrar Mesbah",
     author_email="amesbah@covar.com",
     url="https://github.com/TranslatorSRI/reasoner-pydantic",
